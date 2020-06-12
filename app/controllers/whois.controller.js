6e16f5bba368305b1267fc95161d1553535de336
Alexandr Zani <zani.alexandr@gmail.com>
2020-06-12 17:36:23 +0300

    No domain found functionality.

--- app/controllers/whois.controller.js
+++ app/controllers/whois.controller.js
@@ -2,10 +2,11 @@ const whois = require('whois');
 const metafetch = require('metafetch');
 const db = require("../models");
 const Domain = db.domains;
+const notfound = new RegExp(/(queried object does not exist)|(No match)|(No entries found)|(No Object Found)|(No Data Found)|(not found.)|(NOT FOUND)/);
 
 exports.get = (req, res) => Domain.exists({domain: req.params.id}, function (err, data) {
 
-    console.log(data);
+    console.log(req.params.id, data);
     if (data) {
         Domain.findOne({domain: req.params.id})
             .then(data => {
@@ -21,11 +22,12 @@ exports.get = (req, res) => Domain.exists({domain: req.params.id}, function (err
     else {
         // Create a Tutorial
         whois.lookup(req.params.id, function (err, data) {
+            console.log(req.params.id, data);
             if (data) {
+                let noDomain = data.search(notfound);
+                console.log(noDomain);
+                if (noDomain !== -1) {res.render('nodomain', {data: data, domain: req.params.id}); return; }
                 let meta = metafetch.fetch(req.params.id, function(err, metainfo) {
-                    if (err) {
-                        return console.error(err);
-                    }
                     let domain = new Domain({
                         domain: req.params.id,
                         whois: data,
